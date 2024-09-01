f06b388feb725f8017abaf0c25ceefb52c35af44
Kajal Singh <ks9392656@gmail.com>
2024-09-01 14:48:47 +0530

    convert celsius to fahrenheit

---
+++ 4th_program.py
@@ -0,0 +1,4 @@
+#program to convert kilometers to miles
+km = float(input("Enter the distance:"))
+miles = km * 0.621371
+print(miles)
---
+++ 5th_program.py
@@ -0,0 +1,4 @@
+# program to convert celsius to fahrenheit
+cel = float(input("Enter the degrees in celcius:"))
+fah = (cel*9/5)+32
+print(cel , "celcius is equal to", fah, "fahrenheit")
