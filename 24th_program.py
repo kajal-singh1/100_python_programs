3cefd9571c1cb4d6c73382115134ec4e11278cd9
Kajal Singh <ks9392656@gmail.com>
2024-09-01 15:57:04 +0530

    program to make a simple calculator

---
+++ 24th_program.py
@@ -0,0 +1,17 @@
+#python program to make a simple calculator
+
+inp1 = float(input("Enter the number:"))
+inp2 = float(input("Enter the number:"))
+
+print("press 1 for addition \npress 2 for subtraction \npress 3 for multiplication \npress 4 division ")
+
+choice = int(input("Enter your choice:"))
+
+if choice == 1:
+  print("addition of numbers = " , inp1+inpt2)
+elif choice == 2:
+  print("subtraction of numbers = " , inpt1-inpt2)
+elif choice == 3:
+  print("multiplication of numbers = ", inpt1*inpt2)
+else:
+  print("division of numbers = " , inpt1/inpt2)
