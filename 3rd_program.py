03eb599d04a30b621159de9ae8f80fe904b175fe
Kajal Singh <ks9392656@gmail.com>
2024-09-01 14:36:10 +0530

    generate a random  number

---
+++ 3rd_program.py
@@ -0,0 +1,7 @@
+#program to generate a random number
+import random
+import math
+num1 = int(input("enter the number:"))
+num2 = int(input("enter the number:"))
+output = random.randint(num1,num2)
+print(output)
