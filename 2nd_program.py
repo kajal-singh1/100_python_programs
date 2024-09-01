9e4f1960801d6ac1fc4c8d0a39084f9a7d83db7c
Kajal Singh <ks9392656@gmail.com>
2024-09-01 14:31:49 +0530

    swapping the numbers

---
+++ 2nd_program.py
@@ -0,0 +1,10 @@
+#program for swapping the numbers
+x = 22
+y = 33
+temp = y
+y = x
+x = temp
+print(x)
+print(y)
+print("after swapping y variable =  ", y)
+print("after swapping x variable =  ", x)
