7ccc095c0e86520c68265500f0a650540e3ea79c
Kajal Singh <ks9392656@gmail.com>
2024-09-01 14:43:43 +0530

    check if a number is +ve/-ve/zero

---
+++ 6th_program.py
@@ -0,0 +1,8 @@
+#program to check if a number is positive  or negative or zero
+num = float(input("enter the number:"))
+if num > 0:
+  print("num is positive")
+elif num < 0:
+  print("num is negative")
+else:
+  print("num is zero")
